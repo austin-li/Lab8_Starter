c3a7bebe87274079c57ee9281024c90dd7773e40
Austin Li <austinmingli@gmail.com>
2023-05-23 17:29:33 -0700

    commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Lab 8 - Starter
+
+1. Within a Github action that runs whenever code is pushed, because you want to make sure the code in the git repository is correct
+2. no, it would make more sense to write a unit test
+3. no, it would be hard to check if the other user received the message
+4. yes, if the client is checking the message length before sending the message, then it would be an encapsulated test
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,73 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+// isPhoneNumber
+test('123-456-7890 is phone number', () => {
+    expect(functions.isPhoneNumber('123-456-7890')).toBeTruthy();
+})
+test('456-7890 is phone number', () => {
+    expect(functions.isPhoneNumber('456-7890')).toBeTruthy();
+})
+test('123-456-789 is not a phone number', () => {
+    expect(functions.isPhoneNumber('123-456-789')).toBeFalsy();
+})
+test('1234567890 is not a phone number', () => {
+    expect(functions.isPhoneNumber('1234567890')).toBeFalsy();
+})
+
+// isEmail
+test('a@a.co is an email', () => {
+    expect(functions.isEmail('a@a.co')).toBeTruthy();
+})
+test('2@a.co is an email', () => {
+    expect(functions.isEmail('2@a.co')).toBeTruthy();
+})
+test('a@a.c is not an email', () => {
+    expect(functions.isEmail('a@a.c')).toBeFalsy();
+})
+test('a@2.co is not an email', () => {
+    expect(functions.isEmail('a@2.co')).toBeFalsy();
+})
+
+// isStrongPassword
+test('a_23 is a strong password', () => {
+    expect(functions.isStrongPassword('a_23')).toBeTruthy();
+})
+test('a_2345678901234 is a strong password', () => {
+    expect(functions.isStrongPassword('a_2345678901234')).toBeTruthy();
+})
+test('a_2 is not a strong password', () => {
+    expect(functions.isStrongPassword('a_2')).toBeFalsy();
+})
+test('2_23 is not a strong password', () => {
+    expect(functions.isStrongPassword('2_23')).toBeFalsy();
+})
+
+// isDate
+test('11/11/1111 is a date', () => {
+    expect(functions.isDate('11/11/1111')).toBeTruthy();
+})
+test('1/1/1111 is a date', () => {
+    expect(functions.isDate('1/1/1111')).toBeTruthy();
+})
+test('a11/11/1111 is not a date', () => {
+    expect(functions.isDate('a11/11/1111')).toBeFalsy();
+})
+test('11/11/111 is not a date', () => {
+    expect(functions.isDate('a11/11/111')).toBeFalsy();
+})
+
+// isHexColor
+test('#123456 is a hex color', () => {
+    expect(functions.isHexColor('#123456')).toBeTruthy();
+})
+test('#123 is a hex color', () => {
+    expect(functions.isHexColor('#123')).toBeTruthy();
+})
+test('#12345 is not a hex color', () => {
+    expect(functions.isHexColor('#12345')).toBeFalsy();
+})
+test('#1234567 is not a hex color', () => {
+    expect(functions.isHexColor('#1234567')).toBeFalsy();
+})
